963fd9d0ad872a7ab0858b50ef5859a9811f7992
rockyfu267 <rocky930323@gmail.com>
2020-11-26 10:45:06 +0800

    Update 数组旋转

--- sample/rotate.go
+++ sample/rotate.go
@@ -2,19 +2,7 @@ package sample
 
 //旋转数组
 //给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
-//rotate
-// func rotate(nums []int, k int) {
-// 	lenNum := len(nums)
-// 	for i := range nums {
-// 		if (i + k) >= lenNum {
-
-// 		}
-// 	}
-// }s
-
-// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
-// 要求使用空间复杂度为 O(1) 的 原地 算法。
-
+//rotate 时间94% 空间5%  而且没有用原地算法  空间复杂度为n  用了一组额外的空间
 func rotate(nums []int, k int) (res []int) {
 	lenNum := len(nums)
 	for i := 0; i < lenNum; i++ {
@@ -23,7 +11,12 @@ func rotate(nums []int, k int) (res []int) {
 	for i := 0; i < lenNum; i++ {
 		if (i + k) >= lenNum {
 			n := (i + k) - lenNum
-			res[n] = nums[i]
+			if n < lenNum {
+				res[n] = nums[i]
+			} else {
+				nTMP := n % lenNum
+				res[nTMP] = nums[i]
+			}
 
 		} else {
 			n := i + k
@@ -32,3 +25,6 @@ func rotate(nums []int, k int) (res []int) {
 	}
 	return res
 }
+
+// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+// 要求使用空间复杂度为 O(1) 的 原地 算法。
--- sample/rotate_test.go
+++ sample/rotate_test.go
@@ -7,6 +7,6 @@ import (
 
 func Test_rotate(t *testing.T) {
 	tmpInt := []int{7, 1, 5, 3, 6, 4}
-	res := rotate(tmpInt,1)
+	res := rotate(tmpInt, 11)
 	fmt.Println(res)
 }
