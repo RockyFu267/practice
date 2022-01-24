caaaa3bf974c73353d3ea7c444d2f2aa27046534
RockyFu267 <453618045@qq.com>
2022-01-24 17:00:59 +0800

    test 002

--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("shit")
-	var nums []int
-	nums = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// var nums []int
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	res := searchInsert(nums, 0)
 	fmt.Println(res)
 }
 
 func searchInsert(nums []int, target int) int {
 	for i := range nums {
+		fmt.Println(i)
 		if target <= nums[i] {
 			return i
 		} else {
 			continue
 		}
 	}
-	return len(nums) + 1
+	return len(nums)
 }
