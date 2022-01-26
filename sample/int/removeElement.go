c0f6236b3f2051cd9e3e3f10886d8c20bdaa16f4
RockyFu267 <453618045@qq.com>
2022-01-26 15:24:13 +0800

    version 001

--- sample/int/removeElement.go
+++ sample/int/removeElement.go
@@ -45,13 +45,19 @@ package int
 
 //removeElement	 移除元素
 func removeElement(nums []int, val int) int {
-	var res int
-	for i := range nums {
-		if nums[i] == val {
-			continue
-		} else {
-			res = res + 1
+	right := len(nums) - 1
+	var tmpNum int
+	for left := 0; left < right; left++ {
+		//先判断目前的最后一位是不是等于val,保证右边的不是目标值
+		for nums[right] == right {
+			right = right - 1
+		}
+		//匹配到，完成交换
+		if nums[left] == val {
+			tmpNum = nums[right]
+			nums[right] = nums[left]
+			nums[left] = tmpNum
 		}
 	}
-	return res
+	return right
 }
