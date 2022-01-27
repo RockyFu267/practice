28a9e062248f389236a8ea8f5e939dee9d4592a1
Rockyfu <453618045@qq.com>
2022-01-27 23:19:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,24 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("shit")
-	// var nums []int
-	nums := []int{3, 2, 3, 2, 2, 2}
-	res := removeElement(nums, 2)
-	fmt.Println(res)
 }
 
-func removeElement(nums []int, val int) int {
-	right := len(nums)
-	left := 0
-	for left < right {
-		if nums[left] == val {
-			nums[left] = nums[right-1]
-			right = right - 1
-		} else {
-			left = left + 1
-		}
-		fmt.Println(nums)
-	}
-	fmt.Println(nums)
-	return left
-}
