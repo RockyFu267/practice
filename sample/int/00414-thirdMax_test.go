fe0efe9eb0cec0d9354a4cdccdee218b8096697f
RockyFu267 <453618045@qq.com>
2022-02-05 21:36:29 +0800

    test verison 001

--- sample/int/00414-thirdMax_test.go
+++ sample/int/00414-thirdMax_test.go
@@ -6,17 +6,17 @@ import (
 )
 
 func Test_thirdMax(t *testing.T) {
-	tmpInt := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	tmpInt := []int{3, 2, 1}
 	res := thirdMax(tmpInt)
 	fmt.Println(res)
 	fmt.Println(tmpInt)
 
-	tmpInt01 := []int{5, 7, 4, 6, 3, 1, 2, 9, 8}
+	tmpInt01 := []int{1, 2}
 	res01 := thirdMax(tmpInt01)
 	fmt.Println(res01)
 	fmt.Println(tmpInt01)
 
-	tmpInt02 := []int{4, 9, 5}
+	tmpInt02 := []int{2, 2, 3, 1}
 	res02 := thirdMax(tmpInt02)
 	fmt.Println(res02)
 	fmt.Println(tmpInt02)
