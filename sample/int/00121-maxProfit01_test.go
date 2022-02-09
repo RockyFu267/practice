5d0ea0780d17d3d359a238d118e74bbc814ed0eb
RockyFu267 <453618045@qq.com>
2022-02-09 21:23:37 +0800

    test 002

--- sample/int/00121-maxProfit01_test.go
+++ sample/int/00121-maxProfit01_test.go
@@ -14,8 +14,12 @@ func Test_maxProfit01(t *testing.T) {
 	res01 := maxProfit01(tmpInt01)
 	fmt.Println(res01)
 
-	tmpInt02 := []int{9, 7, 8, 6, 0, 1, 2, 0, 1}
+	tmpInt02 := []int{9, 2, 8, 6, 7, 1, 3, 0, 1}
 	res02 := maxProfit01(tmpInt02)
 	fmt.Println(res02)
+
+	tmpInt03 := []int{8, 9, 8, 9, 5, 8, 6, 7, 1, 6, 0, 1}
+	res03 := maxProfit01(tmpInt03)
+	fmt.Println(res03)
 
 }
