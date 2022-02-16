bd766882be3d171dc3f6e2680ebb6efc04ca426e
RockyFu267 <453618045@qq.com>
2022-02-16 22:11:45 +0800

    add debug

--- sample/int/00036-isValidSudoku.go
+++ sample/int/00036-isValidSudoku.go
@@ -1,5 +1,7 @@
 package int
 
+import "fmt"
+
 // 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
 
 // 数字 1-9 在每一行只能出现一次。
@@ -158,6 +160,7 @@ func isValidSudoku(board [][]byte) bool {
 	tmpRow := map[byte]bool{}
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
+			fmt.Println(i, j)
 			if _, ok := tmpRow[board[i][j]]; ok && string(board[i][j]) != "." {
 				return false
 			}
@@ -167,6 +170,7 @@ func isValidSudoku(board [][]byte) bool {
 	//column
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
+			fmt.Println(i, j)
 			if _, ok := tmpRow[board[j][i]]; ok && string(board[j][i]) != "." {
 				return false
 			}
