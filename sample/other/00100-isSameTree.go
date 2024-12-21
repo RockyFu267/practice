1f9f5d897297821d6478a49eb242ff4c1afdee31
RockyFu267 <453618045@qq.com>
2024-12-21 18:06:54 +0800

    100 相同的树

---
+++ sample/other/00100-isSameTree.go
@@ -0,0 +1,30 @@
+package other
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// isSameTree 相同的树
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
