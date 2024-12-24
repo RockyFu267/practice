a7bde33a498a7de9e9a8c5d68cb638c782550533
RockyFu267 <453618045@qq.com>
2024-12-24 13:26:17 +0800

    94 二叉树 中序遍历

---
+++ sample/other/00094-inorderTraversal.go
@@ -0,0 +1,24 @@
+package other
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// inorderTraversal 中序遍历
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return
+}
